82fb502fe401d4ede470426503c8bb58cd8c6fa9
Morukurthi <morukurthinagalakshmi@gmail.com>
2023-02-22 15:07:42 +0530

    batch5

---
+++ Class.py
@@ -0,0 +1,8 @@
+class Student:
+    name="student"
+    roll="22cse519"
+    km
+    def print_details(self):
+        print(f"name:{self.name} and roll:{self.roll}")
+obj1=Student()
+obj1.print_details()
